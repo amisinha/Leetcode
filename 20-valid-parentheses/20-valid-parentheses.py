e58efe29a7f8ddffaa3f2f2083bf542d48ed7684
Amisha Sinha <32571604+amisinha@users.noreply.github.com>
2022-06-25 14:37:48 +0530

    Time: 35 ms (84.13%), Space: 13.9 MB (23.69%) - LeetHub

---
+++ 20-valid-parentheses/20-valid-parentheses.py
@@ -0,0 +1,22 @@
+class Solution:
+    def isValid(self, s: str) -> bool:
+        st =[]
+        leftb = ["(","{","["]
+        rightb = [")","}","]"]
+        
+        for i in s:
+            if i in leftb:
+                st.append(i)
+            elif i in rightb:
+                if len(st)!= 0 and st[-1] == leftb[rightb.index(i)]:
+                    st.pop()
+                else:
+                    return False
+        return len(st)==0
+       
+      
+                        
+                    
+                    
+            
+        

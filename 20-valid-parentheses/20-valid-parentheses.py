099ba84168272f4ad3fbfb0a6ca2d61c1041e661
Amisha Sinha <32571604+amisinha@users.noreply.github.com>
2022-09-18 01:46:35 +0530

    Time: 62 ms (22.05%), Space: 13.9 MB (26.61%) - LeetHub

--- 20-valid-parentheses/20-valid-parentheses.py
+++ 20-valid-parentheses/20-valid-parentheses.py
@@ -1,18 +1,19 @@
 class Solution:
     def isValid(self, s: str) -> bool:
-        st =[]
-        leftb = ["(","{","["]
-        rightb = [")","}","]"]
+        st = []
+        leftb = [ '(', '{', '[']
+        rightb = [ ')', '}', ']']
         
         for i in s:
             if i in leftb:
                 st.append(i)
             elif i in rightb:
-                if len(st)!= 0 and st[-1] == leftb[rightb.index(i)]:
+                if len(st) is not 0 and st[-1] == leftb[rightb.index(i)]:
                     st.pop()
                 else:
                     return False
-        return len(st)==0
+        return len(st) == 0
+  
        
       
                         
